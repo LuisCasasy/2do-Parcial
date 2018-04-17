930a4ddf3bcff4b6af71612aa480bb9d5510dccb
LuisCasasy <36386636+LuisCasasy@users.noreply.github.com>
2018-04-17 08:45:35 -0500

    Add files via upload

---
+++ .gitignore/GeometricObject.java
@@ -0,0 +1,41 @@
+/**************************
+lUIS ENRIQUE CASAS YAÑWZ<
+GeometricObject.java
+**************************/import java.util.Date; //es para fecha
+
+public abstract class GeometricObject{
+  private String color;
+  private boolean filled; //rellenado
+  private Date dateCreated;//Fecha de creacion
+
+//Methods
+public GeometricObject(){
+  this.color = "purple";
+  this.filled= true;
+  this.dateCreated = new Date();
+}
+public GeometricObject(String color, boolean filled){
+    this.color=color;
+    this.filled= false;
+    this.dateCreated = new Date();
+  }
+  public String getColor(){
+    return this.color;
+  }
+  public void setColor(String color){
+    this.color = color;
+  }
+  public boolean isFilled(){
+    return this.filled;
+  }
+  public void setFiller(boolean filled){
+    this.filled= filled;
+  }
+  public Date getDateCreated(){
+    return this.dateCreated;
+  }
+  //DEfinir el metodo abstracto que puedes utilizar Circle o Rectangle
+  public abstract double getArea();
+  public abstract double getPerimeter();
+
+}
